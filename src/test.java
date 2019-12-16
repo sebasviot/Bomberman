46504f07d2cf4496e38bdf7b63cba541c6f3b833
Guillaume Poiron <guillaume.poiron@outlook.fr>
2019-12-16 16:35:34 +0100

    Test commit - ajout commentaire dans fichier test.java

--- src/test.java
+++ src/test.java
@@ -3,5 +3,7 @@ public class test {
 
 	public static void main(String[] args) throws Exception {
 		InterfaceControleur cbg = new ControleurBombermanGame("src/layouts/alone.lay");
+
+		// Test commit : blablabla
 	}
 }
