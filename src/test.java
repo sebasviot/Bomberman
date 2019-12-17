4333d20543bcfe3493ae902067f962a9addfe4d4
GuillaumePoiron <guillaume.poiron@outlook.fr>
2019-12-17 13:29:34 +0100

    Test - A ne pas prendre en compte

--- src/test.java
+++ src/test.java
@@ -3,5 +3,8 @@ public class test {
 
 	public static void main(String[] args) throws Exception {
 		InterfaceControleur cbg = new ControleurBombermanGame("src/layouts/alone.lay");
+
+
+		System.out.println("saluuuut");
 	}
 }
