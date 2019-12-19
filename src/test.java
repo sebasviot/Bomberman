9e36ba045858d20f9ec1782b06db52087f0b2a32
Sebastien VIOT <sebastien.viot.49@gmail.com>
2019-12-19 09:26:18 +0100

    Bombes explosent
    Les bombes explosent. Elles font perdre une vie à chaque agent qui se trouve sur la range de la bombe, et elles cassent les murs cassables.

--- src/test.java
+++ src/test.java
@@ -2,9 +2,7 @@
 public class test {
 
 	public static void main(String[] args) throws Exception {
-		InterfaceControleur cbg = new ControleurBombermanGame("src/layouts/alone.lay");
-
-
-		System.out.println("saluuuut");
+		InterfaceControleur cbg = new ControleurBombermanGame("src/layouts/niveau1.lay");
 	}
+	
 }
